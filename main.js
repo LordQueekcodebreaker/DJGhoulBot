d76c557756a8f14fa42152ce2f88dbe384a6f0ac
Joost <s1072976@student.windesheim.nl>
2022-04-29 11:57:37 +0200

    main token removed

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const { Client, Intents } = require('discord.js')
 require('dotenv').config()
-const token = 'NDk0NDk4MTIxNDY0Njc2MzYz.W6uHQw.4_cfwygPy2VHSXysVN81moq8miw'
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
