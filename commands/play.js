9f81d042eff8edee673514508299a587ce0b1648
Joost <s1072976@student.windesheim.nl>
2022-05-03 14:32:14 +0200

    Removed_Comments_play

--- commands/play.js
+++ commands/play.js
@@ -11,13 +11,6 @@ module.exports = {
         .setRequired(true)
     ),
   async execute (interaction, player) {
-    // add the trackStart event so when a song will be played this message will be sent
-    // player.on('trackStart', (queue, track) =>
-    //   queue.metadata.channel.send(`🎶 | Now playing **${track.title}**!`)
-    // )
-
-    // await interaction.reply('test')
-
     if (!interaction.member.voice.channelId)
       return await interaction.reply({
         content: 'You are not in a voice channel!',
@@ -61,12 +54,8 @@ module.exports = {
       return await interaction.followUp({
         content: `❌ | Track **${query}** not found!`
       })
-    // if (track.length == 1) {
-    //   queue.addTrack(track)
-    // } else {
+      
     queue.addTracks(track)
-    //}
-
     queue.play()
 
     return await interaction.followUp({
